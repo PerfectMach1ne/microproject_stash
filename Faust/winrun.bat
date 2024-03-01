748c8c051dee798e65114bc5d194e6beae3bb6da
PerfectMach1ne <75753483+PerfectMach1ne@users.noreply.github.com>
2024-03-01 23:29:04 +0100

    Faust: God I love Notepad programming

---
+++ Faust/winrun.bat
@@ -0,0 +1,12 @@
+@echo off
+
+color 0C
+echo Command line guide:
+echo node app.js list :: List all the thingies.
+echo node app.js last :: Display the last thingy.
+echo node app.js add priv "<What you did>" :: Add a thingy to private WagnerDoc.
+echo node app.js add publ "<What you did>" :: Add a thingy to public WagnerDoc.
+echo:
+
+cd /D D:\Programming\microproject_stash\Faust
+cmd

f749c6e04e752e8a8b3923498bc1ebeb764c1f83
starr4ever@TransEdenV4 <starflowerr777@gmail.com>
2024-08-29 01:06:19 +0200

    fixing 2 stupidest bugs ever

--- PoGamochodoro/front/index.js
+++ PoGamochodoro/front/index.js
@@ -7,13 +7,16 @@ class Pomodoro {
   intervalId;
 
   constructor(date, settings) {
-    this.#date = date;
-    this.h = date.getHours();
-    this.m = date.getMinutes() 
-    this.s = date.getSeconds();
-    document.getElementById("pomodoroTimer").innerText = "00:00:00";
     if (settings !== null) {
       this.#date = settings.getTime();
+      this.h = this.#date.getHours();
+      this.m = this.#date.getMinutes() 
+      this.s = this.#date.getSeconds();
+      document.getElementById("pomodoroTimer").innerText = 
+                      String(this.h).padStart(2, '0') + ':' + 
+                      String(this.m).padStart(2, '0') + ':' +
+                      String(this.s).padStart(2, '0');
+      this.s = this.#date.setSeconds(this.s == 0 ? 59 : this.s - 1);
     }
   }
 
@@ -26,13 +29,12 @@ class Pomodoro {
   }
 
   run() {
+    this.#refreshTimer();
     let hourString = String(this.h).padStart(2, '0') + ':' + 
-                    String(this.m).padStart(2, '0') + ':' +
-                    String(this.s).padStart(2, '0');
+                     String(this.m).padStart(2, '0') + ':' +
+                     String(this.s).padStart(2, '0');
     
-    
-    // ToDo: this needs to decrement the time, as opposed to stopwatch functionality.
-    console.log(this.#date);
+    // this.#refreshTimer();
     this.#date.setSeconds(this.#date.getSeconds() - 1);
     this.#refreshTimer();
 
@@ -77,16 +79,16 @@ function digitalClock() {
 
 function start() {
   if (currentPomodoro == null) { // To prevent the button from being used to just restart it. Cease does that!
-    if (currentPomodoro !== null) {
-      clearInterval(currentPomodoro.intervalId);
-      currentPomodoro.intervalId = null;
-    }
+    // if (currentPomodoro !== null) {
+    //   clearInterval(currentPomodoro.intervalId);
+    //   currentPomodoro.intervalId = null;
+    // }
     
     let date = new Date();
     // ToDO: this needs to start from a time passed via Pomodoro settings to the app!!!
     date.setHours(0);
     date.setMinutes(0);
-    date.setSeconds(0 + 1); // It's gotta be 1, as otherwise the "zeroth second lasts two seconds". Odd fix for an odd bug.
+    date.setSeconds(0 - 1); // Odd fix for an odd bug; update: it does absolutely nothing
     date.setMilliseconds(0);
 
     // currentPomodoro = new Pomodoro(date);
@@ -114,7 +116,7 @@ class PomodoroSettings {
 
     this.date.setHours(Number(timeArr[0]));
     this.date.setMinutes(Number(timeArr[1]));
-    this.date.setSeconds(Number(timeArr[2]) + 1); // It's gotta be 1, as otherwise the "zeroth second lasts two seconds". Odd fix for an odd bug.
+    this.date.setSeconds(Number(timeArr[2]));
     this.date.setMilliseconds(0);
 
     this.h = this.date.getHours();
